8750c95994071edca70480da2bd01ba08c50c83c
thiyagu <thiyagu@7612856c-4a0b-11de-acc5-7fb027fc3da9>
2009-07-25 15:21:02 +0000

    ICICI NSE mapping WIP, TransactionSync prints out missing ones

---
+++ src/pm/net/icici/TransactionSynchronizer.java
@@ -0,0 +1,40 @@
+package pm.net.icici;
+
+import pm.dao.ibatis.dao.DAOManager;
+import pm.dao.ibatis.dao.ITransactionDAO;
+import pm.vo.ICICITransaction;
+import pm.vo.TransactionVO;
+
+import java.util.List;
+
+public class TransactionSynchronizer {
+
+    public static void main(String[] args) {
+        new TransactionSynchronizer().sync();
+    }
+
+    public void sync() {
+        ITransactionDAO iTransactionDAO = DAOManager.getTransactionDAO();
+        List<ICICITransaction> iciciTransactions = iTransactionDAO.iciciTransactions();
+        List<TransactionVO> transactions = iTransactionDAO.getTransactionList("ICICI_Direct", null);
+        for (ICICITransaction iciciTransaction : iciciTransactions) {
+            boolean matched = false;
+            for (TransactionVO transaction : transactions) {
+                if (match(iciciTransaction, transaction)) {
+                    matched = true;
+                    break;
+                }
+            }
+            if (!matched) {
+                iciciTransaction.setPortfolio("Thiyagu");
+                iciciTransaction.setTradingAc("ICICI_Direct");
+                System.out.println(iciciTransaction.getDetails());
+            }
+        }
+    }
+
+    private boolean match(ICICITransaction i1, TransactionVO t1) {
+        return i1.getStockCode().equals(t1.getStockCode()) && i1.getDate().equals(t1.getDate()) && i1.getAction() == t1.getAction()
+                && i1.getPrice() == t1.getPrice();
+    }
+}
