b95c0c69f3a55ffddc8e7159be373cef5630369c
thiyagu <thiyagu@7612856c-4a0b-11de-acc5-7fb027fc3da9>
2009-12-06 22:17:03 +0000

    revert

--- src/pm/net/nse/downloader/AbstractFileDownloader.java
+++ src/pm/net/nse/downloader/AbstractFileDownloader.java
@@ -22,7 +22,6 @@ public abstract class AbstractFileDownloader extends AbstractDownloader {
     }
 
     public void run() {
-        breaking build
         logger.info("Downloading " + getFileType() + " file : " + date);
         InputStream reader = getHTTPHelper().getDataStream(getThisURL());
         if (reader != null) {
